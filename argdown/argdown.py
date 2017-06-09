c881745ee226472a9d478bfdbce9ca1f5c1b69f7
Rebecca Turner <637275@gmail.com>
2017-06-08 20:55:49 -0400

    console work

--- argdown/argdown.py
+++ argdown/argdown.py
@@ -5,11 +5,13 @@ import os as _os
 import textwrap as _textwrap
 
 version = '1.0.0'
+cols = _os.environ['COLUMNS'] if 'COLUMNS' in _os.environ else 80
 
 def md_help(parser, *, depth=1, header='Arguments and Usage',
         usage_header='Usage', ref_header='Quick reference table',
         args_header='Arguments', spacey=False, show_default=True,
         truncate_help=True):
+    global cols
     space = '\n' if spacey else ''
     out = ('#' * depth + f' {header}\n{space}'
         +  '#' * (depth + 1)
@@ -19,7 +21,6 @@ def md_help(parser, *, depth=1, header='Arguments and Usage',
         +  '#' * (depth + 2) + f' {ref_header}\n{space}')
 
     used_actions = {}
-    cols = _os.environ['COLUMNS'] if 'COLUMNS' in _os.environ else 80
     args_detailed = ''
 
     options = []
@@ -114,4 +115,102 @@ def md_help(parser, *, depth=1, header='Arguments and Usage',
     return out
 
 def console():
-    print('hii')
+    prog = 'argdown'
+    global cols
+
+    argparser = argparse.ArgumentParser(
+        description='Markdown export for the argparse module',
+        epilog='More info: github.com/9999years/argdown',
+        prog=prog
+    )
+
+    argparser.add_argument('src_file', nargs='*',
+        help='The filename of a Python file to export Markdown from.')
+
+    argparser.add_argument('-', action='store_true', dest='use_stdin',
+        help='Read from STDIN instead of a file.')
+
+    argparser.add_argument('--license', action='store_true',
+        help='Print license information (MIT) and exit.')
+
+    argparser.add_argument('--usage-header', type=str, default='Usage',
+            help='Header text for the `Usage` section.')
+
+    argparser.add_argument('--ref-header', type=str, default='Quick '
+            'reference table', help='Header text for the `Quick reference '
+            'table` section, a simple table of all the arguments.')
+
+    argparser.add_argument('--args-header', type=str,
+            default='Arguments', help='Header text for the `Arguments` '
+            'section, a detailed listing of all the arguments.')
+
+    argparser.add_argument(['-s', '--spacey'], action='store_true',
+            help='Output a blank line after headers.')
+
+    argparser.add_argument(['-h', '--hide-default'], action='store_true',
+            help='Don\'t output default values for the arguments.')
+
+    argparser.add_argument(['-t', '--truncate-help'], action='store_true',
+        help='Truncate help in the `Quick reference table` section so that '
+        'the table\'s width doesn\'t exceed `--width`. Makes terminal output '
+        'prettier but means you\'ll probably have to re-write help messages.')
+
+    argparser.add_argument('--header-depth', type=int, default=1,
+        help='Header depth; number of hashes to output before the '
+        'top-level header.')
+
+    argparser.add_argument('-v', '--version', action='version',
+        version=f'%(prog)s {version}')
+
+    args = argparser.parse_args()
+
+    if args.license:
+        print('''Copyright (c) 2017 Rebecca Turner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.''')
+        exit()
+
+    # def md_help(parser, *, depth=1, header='Arguments and Usage',
+            # usage_header='Usage', ref_header='Quick reference table',
+            # args_header='Arguments', spacey=False, show_default=True,
+            # truncate_help=True):
+
+    usage_header  = args.usage_header
+    ref_header    = args.ref_header
+    args_header   = args.args_header
+    spacey        = args.spacey
+    show_default  = not args.hide_default
+    truncate_help = args.truncate_help
+    depth         = args.header_depth
+
+    def gen_help(src):
+        md_help()
+
+    if args.use_stdin:
+        # catenate stdinput, parse / render
+        src = ''
+        for line in sys.stdin:
+            src += line + '\n'
+
+    # process each file, respecting encoding, although i really hope nobody ever
+    # uses that argument and to be quite frank i haven't tested it
+    for fname in args.src_file:
+        with open(fname, 'r', encoding=args.encoding) as f:
+            tree = parser.parsed_data
+            print(treetomd(tree, numbering=numbering))
